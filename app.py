8361f0dd4b061ecb0c2cefca6af7f6ffa81571da
apoorv-x12 <76410421+apoorv-x12@users.noreply.github.com>
2024-07-14 08:02:52 +0530

    added error handling
    added error handling

--- app.py
+++ app.py
@@ -3,30 +3,36 @@ import subprocess
 import commands as com
 import colors 
 
+
+  
 if '__main__' == __name__:
-    while True:
-        user_prompt = input(f"{colors.Colors.GREEN}>>>> {colors.Colors.RESET}")
-        parsed=user_prompt.split(' ')
-    
-        if parsed[0] == '' :
-            print(f"{colors.Colors.RED}Give a non-empty command{colors.Colors.RESET}")
-            continue
-        if len(parsed)-1 >= len(com.commands) :
-            print(f"{colors.Colors.CYAN}Give a valid command{colors.Colors.RESET}")
-            continue
-        if parsed[0] not in com.commands[len(parsed)-1] :
-            print(f"{colors.Colors.YELLOW}Give a valid command{colors.Colors.RESET}")
-            continue
-        
-        # len of args not command aka parsed[0]
-        length=len(parsed)-1
-        command=parsed[0]
-        args= parsed[1:] if length>0 else []
-        
-        # retrieve object from commands list
-        args_list= com.commands[length] 
-        if command in args_list :
-            args_list[command](args)
+        while True:
+            try:   
+                user_prompt = input(f"{colors.Colors.GREEN}>>>> {colors.Colors.RESET}")
+                parsed=user_prompt.split(' ')
             
+                if parsed[0] == '' :
+                    print(f"{colors.Colors.RED}Give a non-empty command{colors.Colors.RESET}")
+                    continue
+                if len(parsed)-1 >= len(com.commands) :
+                    print(f"{colors.Colors.CYAN}Give a valid command{colors.Colors.RESET}")
+                    continue
+                if parsed[0] not in com.commands[len(parsed)-1] :
+                    print(f"{colors.Colors.YELLOW}Give a valid command{colors.Colors.RESET}")
+                    continue
+                
+                # len of args not command aka parsed[0]
+                length=len(parsed)-1
+                command=parsed[0]
+                args= parsed[1:] if length>0 else []
+                
+                # retrieve object from commands list
+                args_list= com.commands[length] 
+                if command in args_list :
+                    args_list[command](args)
+                    
+            except Exception as e:
+                 print(colors.Colors.GREEN + str(e) + colors.Colors.RESET)       
+                    
 
         
--- commands.py
+++ commands.py
@@ -6,15 +6,21 @@ def list_directories(args):
     :param args(list): A list of arguments passed to the function. This parameter is not used in the function and can be ignored.
     :prints: A list of strings representing the names of the files and directories in the current working directory.
     """
-    print(colors.Colors.BLUE + str(os.listdir(os.getcwd())) + colors.Colors.RESET)
-    
+    try:
+        print(colors.Colors.BLUE + str(os.listdir(os.getcwd())) + colors.Colors.RESET)
+    except Exception as e:
+        print(colors.Colors.GREEN + str(e) + colors.Colors.RESET)
+            
 def current_working_directory(args):
     """
     Retrieves the current working directory.
     Args: args(list): The command line arguments passed to the function.
     prints str: The current working directory.
     """
-    print(colors.Colors.YELLOW + os.getcwd() + colors.Colors.RESET)
+    try:
+        print(colors.Colors.YELLOW + os.getcwd() + colors.Colors.RESET)
+    except Exception as e:
+        print(colors.Colors.GREEN + str(e) + colors.Colors.RESET)
 
 def clear(args):
     """
@@ -22,8 +28,22 @@ def clear(args):
     Args: args(list): The command line arguments passed to the function.
     Returns:  None
     """
-    os.system('clear' if os.name == 'posix' else 'cls')
+    try:
+        os.system('clear' if os.name == 'posix' else 'cls')
+    except Exception as e:
+        print(colors.Colors.GREEN + str(e) + colors.Colors.RESET)
+    
 
+def make_directory_in_current_working_directory(args):
+    """
+    Creates a new directory based on the argument provided.
+    Args:args(list): A list containing the name of the directory to be created.    
+    Raises: OSError: If the directory already exists or if there is a permission issue.
+    """
+    try:
+        os.mkdir(args[0])
+    except Exception as e:
+        print(colors.Colors.GREEN + str(e) + colors.Colors.RESET)
 
 
 commands=[
@@ -32,5 +52,8 @@ commands=[
     'cwd': current_working_directory,
     'clr': clear
 },
+{
+    'mkdirincwd': make_directory_in_current_working_directory
+}
           
 ]
